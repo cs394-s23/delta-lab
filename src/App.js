404630de6e4afc10bad8b5bbd45930649f04af41
tanyapetriv <tatyanapetriv2023@u.northwestern.edu>
2023-05-08 20:10:04 -0500

    get top 5 resources

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import UserInput from './components/UserInput';
 import FormBox from './components/FormBox';
 import './App.css';
 import {getResourcesBySkill, getResourceByPath} from './firebase.js';
+import { getTop5Resources } from './utilities';
 
 function App() {
+
+  console.log(getTop5Resources(["0", "1", "7"]));
 
   return (
     <div>
--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,44 @@
+import { getResourcesBySkill } from "./firebase";
 /*
 props.top = [1,2,3]
 
 res = getResourcesBySkill(1,2,3);  --> list of paths
 
+scores = {}
+
+for skill in skills
+    for res in getresourcesbyskill(skill)
+        if scores[res]
+            scores[res] ++
+        else
+            scores[res] = 1
+
 */
-export async function getTop5Resources(props) {
-    
+export async function getTop5Resources(skills) {
+    var scores = {};
+    skills.map(async (skill) => {
+        let resources = await getResourcesBySkill(skill);
+        resources.map( (res) => {
+            if (scores[res]) {
+                scores[res] +=1;
+            } else {
+                scores[res] = 1;
+            }
+        })
+    })
+
+    const sortedScores = Object.entries(scores)
+        .sort((a, b) => b[1] - a[1])
+        .reduce((obj, [key, value]) => {
+            obj[key] = value;
+            return obj;
+    }, {});
+
+    console.log("sorted scores", sortedScores);
+    const top5 = Object.keys(sortedScores).slice([0, Math.min(4, sortedScores.length)]);
+
+    console.log("top5");
+    console.log(top5);
+
+    return top5;
 }
