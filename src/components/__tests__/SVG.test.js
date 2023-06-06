40347539ff089f1696b0f1d731797e6c11c9ea73
Callum Bondy <callum.bondy@gmail.com>
2023-06-06 10:24:32 -0500

    unit tests

--- src/components/__tests__/SVG.test.js
+++ src/components/__tests__/SVG.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import TriangleGraph from '../TriangleGraph';
+import DeltaPopUp from '../DeltaPopUp';
 
 describe('Triangle SVG', () => {
   it('renders App component', () => {
@@ -11,5 +12,11 @@ describe('Triangle SVG', () => {
   it('does not render when data is not provided', () => {
     render(<TriangleGraph />);
     expect(screen.queryByTestId('triangle-svg')).not.toBeInTheDocument();
+  });
+});
+
+describe('DeltaPopUp', () => {
+  it('renders popup', () => {
+    render(<DeltaPopUp category="Professionalism" skills = "Multitasking" color = "#000000"/>);
   });
 });
