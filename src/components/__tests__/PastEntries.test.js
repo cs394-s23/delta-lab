379705b31d5ea683ab20181c9d48a111a2f0a17f
Max Lou <maxlou188@gmail.com>
2023-06-07 19:53:35 -0400

    max tests

--- src/components/__tests__/PastEntries.test.js
+++ src/components/__tests__/PastEntries.test.js
@@ -1,6 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import LongMenu from '../PastEntries';
 
+
+describe('SlidersComponent', () => {
+  it('saves user data from sliders', () => {
+    // Simulate setting the values of sliders
+    const slider1Value = 50;
+    const slider2Value = 75;
+
+    // Simulate clicking the save button
+    const saveButtonClicked = true;
+
+    // Simulate the saveUserData function (does nothing)
+    const saveUserData = jest.fn();
+
+    // Assert that the saveUserData function is called
+    expect(saveUserData).toHaveBeenCalledTimes(0);
+  });
+});
+
 jest.mock('../../context/AuthContext', () => ({
   useUser: () => ({
     user: { uid: 'mockUserId' },
